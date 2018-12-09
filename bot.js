4023a699379a59267623135d27751d29cedb240c
Joey Clover <joey.clover@gmail.com>
2018-12-09 23:53:23 +0000

    Add mongo integration

--- bot.js
+++ bot.js
@@ -2,16 +2,54 @@ const Discord = require('discord.io')
 const logger = require('winston')
 const teams = require('./teams')
 
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert')
+
+
+// Configure DB/Node
+process.stdin.resume()
+
+const url = process.env.MONGO_URI
+const dbName = 'sneak-scrims-labs'
+const mClient = new MongoClient(url)
+var mDB
+
+mClient.connect(function(err) {
+  assert.strictEqual(null, err, 'Failed to connect to DB')
+  logger.info('Connected successfully to DB')
+
+  mDB = mClient.db(dbName)
+})
+
+function exitHandler(options, exitCode) {
+  options, exitCode
+  mClient.close()
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null, { cleanup:true }))
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, { exit:true }))
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null, { exit:true }))
+process.on('SIGUSR2', exitHandler.bind(null, { exit:true }))
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, { exit:true }))
+
+
 // Configure logger settings
 logger.remove(logger.transports.Console)
 logger.add(new logger.transports.Console, {
   colorize: true,
 })
-
 logger.level = 'debug'
 
-// Initialize Discord Bot
-// The Sneak Team Scrim bot responds to `!t` by default
+
+// Configure Bot settings
+
 var bot = new Discord.Client({
   token: process.env.BOT_TOKEN,
   autorun: true,
@@ -39,7 +77,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     }
     else if (cmd === 'create') {
       const teamName = getArgument(args, 0)
-      const err = teams.create(teamName, user, userID)
+      const err = teams.create(mDB, teamName, user, userID)
 
       if (err !== null) {
         bot.sendMessage({ to: channelID, message: `Could not create team. ${err}` })
--- teams.js
+++ teams.js
@@ -1,6 +1,6 @@
 const logger = require('winston')
 
-module.exports.create = (teamName, user, userID) => {
+module.exports.create = (db, teamName, user, userID) => {
   logger.info('Create Team')
   logger.info(teamName + ' ' + user + ' ', userID)
 
@@ -8,5 +8,21 @@ module.exports.create = (teamName, user, userID) => {
     return new Error('no team name provided')
   }
 
+  insertDocuments(db, (r) => {
+    logger.info(r)
+  })
+
   return null
+}
+
+
+const insertDocuments = (db, callback) => {
+  // Get the documents collection
+  const collection = db.collection('teams')
+  // Insert some documents
+  collection.insertMany([
+    { a : 1 }, { a : 2 }, { a : 3 },
+  ], function(err, result) {
+    callback(result)
+  })
 }
